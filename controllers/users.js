03b929415bfdb62e158fa3cc374e5aa8b36a612e
Prnce Tyagi <princetyagi1901@gmail.com>
2024-09-13 16:47:30 +0530

    get user by id

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const data = require('../data.json')
 
-exports.getAllUsers = async  (req, res) => {
+exports.getAllUsers = async (req, res) => {
     try {
-      
+
         await res.json(data)
 
+    } catch (error) {
+        console.log(error)
+    }
+}
+exports.getuser = async (req, res) => {
+    try {
+        let id = await parseInt(req.params.id);
+        let user =  data.find((user) => user.id === id);
+        await res.json(user)
     } catch (error) {
         console.log(error)
     }
--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller =require('../controllers/users');
 router.get("/getalluser", controller.getAllUsers)
+router.get("/getuser/:id", controller.getuser)
+
 
 module.exports=router;
